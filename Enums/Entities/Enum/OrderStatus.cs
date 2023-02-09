67af3bf53574e9845e5e6138465850524975096e
Pedro Graff <pedrograff01@gmail.com>
2023-02-09 10:29:36 -0300

    Versao zerada

---
+++ Enums/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums.Entities.Enum
+{
+    enum OrderStatus : int
+    {
+        Pending_Payment = 0,
+        Processing = 1,
+        Shipping = 2,
+        Delivered = 3
+    }
+}
